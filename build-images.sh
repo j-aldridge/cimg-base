0c65ec3dcae315195a8910c28e1954f2ec1b291c
j-aldridge <jonathan_aldridge@aainsurance.co.nz>
2024-12-23 11:41:27 +1300

    update dockerfile with Hilux apps

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker context create cimg
-docker buildx create --use cimg
-docker buildx build --file 20.04/Dockerfile -t cimg/base:20.04 -t cimg/base:20.04 --platform=linux/amd64,linux/arm64 .
-docker buildx build --file 22.04/Dockerfile -t cimg/base:22.04 -t cimg/base:22.04 --platform=linux/amd64,linux/arm64 .
+docker build --file 22.04/Dockerfile -t cimg/base:22.04 -t cimg/base:22.04 --platform linux/amd64 .
